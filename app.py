c24e97b17fed5f050afef043d6af4f89b51a49ad
LAN YUSHI <yslan@users.noreply.huggingface.co>
2024-11-29 02:23:33 +0000

    Update app.py

--- app.py
+++ app.py
@@ -85,6 +85,8 @@ elif torch.backends.mps.is_available():
     device = torch.device("mps")
 else:
     device = torch.device("cpu")
+    device = torch.device("cuda")
+    print(f"Force device to {device} due to ZeroGPU")
 print(f"using device: {device}")
 
 # segmentation model
